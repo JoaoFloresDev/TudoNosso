44c433bc2d22cc2fe950fa6b09f26de020940bb2
Joao Victor Flores <j199818@dac.unicamp.br>
2019-11-04 10:00:26 -0300

    ViewControllers separados

---
+++ TudoNosso/TudoNosso/ViewControllers/StoriesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StoriesViewController.swift
+//  TudoNosso
+//
+//  Created by Joao Flores on 04/11/19.
+//  Copyright © 2019 Joao Flores. All rights reserved.
+//
+
+import UIKit
+
+class StoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
