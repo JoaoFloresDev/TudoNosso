52cccdfb06f854a5b0135d2afab219e573791c03
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2019-11-04 10:14:51 -0300

    Auto stash before merge of "master" and "origin/master"
    firebase adicionado e Configurado

---
+++ TudoNosso/Pods/Target Support Files/Pods-TudoNosso-TudoNossoUITests/Pods-TudoNosso-TudoNossoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TudoNosso_TudoNossoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TudoNosso_TudoNossoUITests
+@end
---
+++ TudoNosso/Pods/Target Support Files/Pods-TudoNosso/Pods-TudoNosso-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TudoNosso : NSObject
+@end
+@implementation PodsDummy_Pods_TudoNosso
+@end
