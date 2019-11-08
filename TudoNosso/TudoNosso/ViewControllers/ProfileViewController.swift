de562f5804095edd3bbb403773f6938c77e09b60
Priscila Zucato <priscila.zucato@mblabs.com.br>
2019-11-08 11:12:33 -0300

    Segmented control changing between table views.

--- TudoNosso/TudoNosso/ViewControllers/ProfileViewController.swift
+++ TudoNosso/TudoNosso/ViewControllers/ProfileViewController.swift
@@ -14,14 +14,15 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var profileImage: RoundedImageView!
     @IBOutlet weak var segmentedControl: CustomSegmentedControl!
     @IBOutlet weak var jobsTableView: UITableView!
+    @IBOutlet weak var profileTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupTableView()
+        setupJobsTableView()
     }
     
-    func setupTableView(){
+    func setupJobsTableView(){
         jobsTableView.backgroundColor = .clear
         
         jobsTableView.delegate = self
@@ -30,30 +31,58 @@ class ProfileViewController: UIViewController {
         jobsTableView.register(JobsTableViewCell.nib, forCellReuseIdentifier: JobsTableViewCell.reuseIdentifer)
         jobsTableView.register(JobsTableViewHeader.nib, forHeaderFooterViewReuseIdentifier: JobsTableViewHeader.reuseIdentifer)
     }
+
+    @IBAction func segmentChanged(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            self.jobsTableView.isHidden = false
+            self.profileTableView.isHidden = true
+        case 1:
+            self.jobsTableView.isHidden = true
+            self.profileTableView.isHidden = false
+        default:
+            self.jobsTableView.isHidden = false
+            self.profileTableView.isHidden = true
+        }
+    }
 }
 
 extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        switch tableView {
+        case jobsTableView:     return 2
+        case profileTableView:  return 1
+        default:                return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: JobsTableViewHeader.reuseIdentifer) as? JobsTableViewHeader else {
-            fatalError("The dequeued header is not an instance of JobsTableViewHeader.")
+        switch tableView {
+        case jobsTableView:
+            
+            guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: JobsTableViewHeader.reuseIdentifer) as? JobsTableViewHeader else {
+                fatalError("The dequeued header is not an instance of JobsTableViewHeader.")
+            }
+            
+            switch section {
+            case 0:  header.configure(type: .ongoing)
+            case 1:  header.configure(type: .finished)
+            default: break
+            }
+            
+            return header
+            
+        default:    return nil
         }
-        
-        switch section {
-        case 0:  header.configure(type: .ongoing)
-        case 1:  header.configure(type: .finished)
-        default: break
-        }
-        
-        return header
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return JobsTableViewHeader.height
+        switch tableView{
+        case jobsTableView:     return JobsTableViewHeader.height
+        case profileTableView:  return 0
+        default:                return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -61,12 +90,17 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: JobsTableViewCell.reuseIdentifer, for: indexPath) as? JobsTableViewCell else {
-            fatalError("The dequeued cell is not an instance of JobsTableViewCell.")
+        
+        switch tableView{
+        case jobsTableView:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: JobsTableViewCell.reuseIdentifer, for: indexPath) as? JobsTableViewCell else {
+                fatalError("The dequeued cell is not an instance of JobsTableViewCell.")
+            }
+            //todo config cell
+            cell.configureCell()
+            cell.backgroundColor = .clear
+            return cell
+        default: return UITableViewCell()
         }
-        //todo config cell
-        cell.configureCell()
-        cell.backgroundColor = .clear
-        return cell
     }
 }
